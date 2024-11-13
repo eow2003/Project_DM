3a16e3350942783d85a9028135c506aa4fa4b072
Khoa Tap Hat <khoai50km@gmail.com>
2024-11-13 15:00:40 +0700

    Commnit 13/11/2024: first data

--- src/cmd/OriginalCsv2ArffCommand.java
+++ src/cmd/OriginalCsv2ArffCommand.java
@@ -3,9 +3,9 @@ package cmd;
 import util.Converter;
 
 public class OriginalCsv2ArffCommand implements Command {
-	public final static String ORIGINAL_CSV_DATASET = "data/HepatitisCdata.csv";
+	public final static String ORIGINAL_CSV_DATASET = "data/auto-mpg.csv";
 
-	public final static String ORIGINAL_ARFF_DATASET = "data/HepatitisCdata.arff";
+	public final static String ORIGINAL_ARFF_DATASET = "data/auto-mpg.arff";
 
 	public void exec() {
 		Converter.csv2Arff(ORIGINAL_CSV_DATASET, ORIGINAL_ARFF_DATASET);
